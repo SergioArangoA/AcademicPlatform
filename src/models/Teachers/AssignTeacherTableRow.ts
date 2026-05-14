d22420e0330b33aa191464f156a783bff4291dc5
mykeprojects <mykeprojects@gmail.com>
2026-05-13 23:33:26 -0500

    Se hacen correcciones en métodos relacionados con asignar profesores para mejorar la visualización de la pagina

--- src/models/Teachers/AssignTeacherTableRow.ts
+++ src/models/Teachers/AssignTeacherTableRow.ts
@@ -1,12 +1,10 @@
 export interface AssignTeacherTableRow {
     id: string;
     subject_id: string;
-    career_id: string;
     teacher_id: string;
     group_code: string;
     group_name: string;
     subject_name: string;
     subject_code: string;
-    career_name: string;
     teacher_name: string;
 }
