13e631e75c892cd4ef86a0fd9dc6550ec9d66d0c
Sergio <sergio.arango63723@ucaldas.edu.co>
2026-05-15 23:38:24 -0500

    Commit antes de realizar la automatasion

--- src/components/DropdownForm.tsx
+++ src/components/DropdownForm.tsx
@@ -20,10 +20,12 @@ export default function DropdownForm<T>({
   return (
     <div className="flex flex-col gap-2">
 
-      <label className="text-sm font-medium">
+      {/* Label */}
+      <label className="text-sm font-medium text-gray-900 dark:text-gray-200">
         {title}
       </label>
 
+      {/* Select */}
       <select
         value={value}
         onChange={(e) => onChange(e.target.value)}
@@ -32,12 +34,26 @@ export default function DropdownForm<T>({
           rounded-xl
           border
           border-gray-300
+
           bg-white
+          text-gray-900
+
+          dark:bg-gray-800
+          dark:text-gray-100
+          dark:border-gray-600
+
           px-4
           py-2
+
+          focus:outline-none
+          focus:ring-2
+          focus:ring-blue-500
         "
       >
-        <option value="">
+        <option
+          value=""
+          className="text-gray-500 dark:text-gray-400"
+        >
           Seleccione una opción
         </option>
 
@@ -49,6 +65,7 @@ export default function DropdownForm<T>({
             {String(option[labelKey])}
           </option>
         ))}
+
       </select>
 
     </div>
